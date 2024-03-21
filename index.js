55717bb9e5999f3c1eff067e1326f171a1a81645
INFANTJOY <infantjoy123@gmail.com>
2024-03-21 12:01:52 +0530

    npm server completed

---
+++ index.js
@@ -0,0 +1,53 @@
+// import express from 'express';
+// import { createFileAndWriteToFile, getFilesInFolder } from './files.js';
+// import path from 'path';
+
+// const app = express();
+// const folderPath = path.join(_dirname, './files');
+
+// app.get('/create', (req, res) => {
+//     createFileAndWriteToFile();
+//     res.send('File created successfully');
+// });
+// app.get('/files', async (req, res) => {
+//     try {
+//       const files = await getFilesInFolder(folderPath);
+//       res.json(files);
+//     } catch (err) {
+//       res.status(500).json({ error: 'Internal server error' });
+//     }
+//   });
+
+// const port = 3000;
+// app.listen(port, () => {
+//     console.log(Server running on port: http://localhost:${port});
+// });
+
+import express from 'express';
+import { createFileAndWriteToFile, getFilesInFolder } from './files.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const _dirname = path.dirname(_filename);
+const folderPath = path.join(__dirname, './files');
+
+app.get('/create', (req, res) => {
+    createFileAndWriteToFile();
+    res.send('File created successfully');
+});
+
+app.get('/files', async (req, res) => {
+    try {
+      const files = await getFilesInFolder(folderPath);
+      res.json(files);
+    } catch (err) {
+      res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server running on port: http://localhost:${port}`);
+});
